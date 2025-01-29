1dd9bae1ae7254f2f75438b7019c6efe3a72a590
Stathis Kolovos <stathiskolovos@MacBook.local>
2025-01-29 17:55:50 +0200

    last commit

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,12 +5,45 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      totalRatings:0,
+
+      handleLike:() => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      },
+
+      handleDislike:() => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      }
+    };
   }
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+      <div className='content-rating'>
+        <p>
+          React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library
+          that aims to make building user interfaces based on components more "seamless".
+          It is maintained by Meta (formerly Facebook) and a community of individual developers and companies
+        </p>
+        <div className='rating-buttons'>
+          <button className="like-button" onClick={this.state.handleLike}>
+            Like ({this.state.likes})
+          </button>
+          <button className="dislike-button" onClick={this.state.handleDislike}>
+            Dislike ({this.state.dislikes})
+          </button>
+          <p>Total Ratings: {this.state.totalRatings}</p>
+        </div>
+      </div>
     );
   }
 }
